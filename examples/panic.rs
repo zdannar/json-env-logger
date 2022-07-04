0b7228aab9bfeaea584460a486c09fc6a498d754
Zack Dannar <zack.dannar@onxmaps.com>
2022-07-04 13:13:37 -0600

    update

--- examples/panic.rs
+++ examples/panic.rs
@@ -7,10 +7,10 @@
 use kv_log_macro::{debug, error, info, trace, warn};
 
 fn main() {
-    json_env_logger::init();
-    json_env_logger::panic_hook();
+    json_env_logger2::init();
+    json_env_logger2::panic_hook();
     trace!("I am a trace", {task_id: "567", thread_id: "12"});
-    debug!("I am a debug", { foo: 1 });
+    debug!("I am a debug {:?}", { foo: 1 });
     info!("I am an info");
     warn!("I am a warning");
     error!("I am an error");
